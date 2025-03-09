ef521bb02b390df4fe0c13e22de2835d26ee4c7e
Kaiyuan Han <hermit_han@163.com>
2025-03-10 00:49:55 +0800

    Initial commit

---
+++ subread.info.lisp
@@ -0,0 +1,23 @@
+(:name           "subread")
+(:repo           "taffish-org/subread")
+(:author         "KaiyuanHan")
+(:version        "2.0.2")
+(:intro          "The Subread software package is a tool kit for processing next-gen sequencing data. It includes Subread aligner, Subjunc exon-exon junction detector and featureCounts read summarization program.")
+(:dependencies)
+(:functions      "cell-counts"
+                 "detectionCall"
+                 "exactSNP"
+                 "featureCounts"
+                 "flattenGTF"
+                 "genRandomReads"
+                 "propmapped"
+                 "qualityScores"
+                 "removeDup"
+                 "repair"
+                 "subindel"
+                 "subjunc"
+                 "sublong"
+                 "subread-align"
+                 "subread-buildindex"
+                 "subread-fullscan"
+                 "txUnique")
